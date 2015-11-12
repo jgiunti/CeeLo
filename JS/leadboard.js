a514ed26995ac2fc128ff8b1e8b2c0eaf820c4f5
Joe Giunti <jg4229@gmail.com>
2015-11-12 13:07:13 -0500

    intial

--- JS/leadboard.js
+++ JS/leadboard.js
@@ -1,22 +1,13 @@
 $(document).ready(function()
-{                
+{              
     $('#leaderBoard').css({'visibility':'visible'});   
-    $.ajax({
-        url: "../php/leaderBoard.php",
-        dataType: 'json',
-        success: function(jsonData) 
-        {	
-            $.each(jsonData, function()
-            {
-                $('#leaderBoard').append("<tr><td>" + jsonData[0][0] + "</td><td>" + jsonData[0][1] +"</td></tr>"); 
-            });  
-            alert('error');
-        },
-        error: function(json_Data)
-        {
-            alert('Connection Error');
-        }
-    });   
+    
+    $.getJSON( '../php/leaderboard.php', function( data ) 
+    {
+        $.each( data, function( data ) {
+          $('#leaderBoard').append("<tr><td>" + data[0][0] + "</td><td>" + data[0][1] +"</td></tr>"); 
+        });       
+     });         
 });
 
 
