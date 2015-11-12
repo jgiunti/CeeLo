e8834703d46c0956013457f5e1f5fdf110200571
Joe Giunti <jg4229@gmail.com>
2015-11-12 11:06:05 -0500

    intial

--- php/account.php
+++ php/account.php
@@ -4,23 +4,42 @@
     <head>
         <meta charset="UTF-8">
         <title>Account Page</title>
+        <link type="text/css" rel="stylesheet" href="../CSS/styles.css" />
     </head>
     <body>
         <?php 
         require_once 'User.php';
         $user = new User(1);
-        echo $user->userName;
-        echo $user->points;
-        echo $user->wins;
-        echo $user->loss;
         ?>
-        <table id="userInfo">
-            <tr>
-                <th>User Name</th>
-                <th>Total Points</th> 
-                <th>Wins</th>
-                <th>Losses</th>
-            </tr>
-        </table>             
+        
+        <div id="user">
+            <div class="titleAcct">
+                <h1> User Info.</h1>
+            </div>
+            <table id="userInfo">
+                <tr>
+                    <th>User Name</th>
+                    <th>Total Points</th> 
+                    <th>Wins</th>
+                    <th>Losses</th>
+                </tr>
+                <?php 
+                echo '<tr>';
+                    echo '<td>';
+                        echo $user->userName;
+                    echo '</td>';
+                    echo '<td>';
+                        echo $user->points;
+                    echo '</td>';
+                    echo '<td>';
+                        echo $user->wins;
+                    echo '</td>';
+                    echo '<td>';
+                        echo $user->loss;
+                    echo '</td>';
+                echo '</tr>';               
+                ?>
+            </table>             
+        </div>
     </body>
 </html>
